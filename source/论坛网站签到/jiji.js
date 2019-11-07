bf6a67968157cba0460fb8c335b06ab083bd28e6
Wenmoux <45694869+Wenmoux@users.noreply.github.com>
2019-11-07 14:29:08 +0800

    Update jiji.js

--- source/论坛网站签到/jiji.js
+++ source/论坛网站签到/jiji.js
@@ -1,7 +1,7 @@
 log("jiji开始签到");
 
 var url = "https://ji-c.pw/user/checkin";
-var cookie = cookie1;
+var cookie = "cookie1";
 var headers = {
 "User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.87 Safari/537.36";
 "cookie":cookie1
