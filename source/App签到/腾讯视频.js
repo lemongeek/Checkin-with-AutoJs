5d0f09a2b0ab60212d76fa2dd349398ed623dd2e
Wenmoux <45694869+Wenmoux@users.noreply.github.com>
2019-11-07 13:26:16 +0800

    Update 腾讯视频.js

--- source/App签到/腾讯视频.js
+++ source/App签到/腾讯视频.js
@@ -1,4 +1,4 @@
-var cookie = " " //cookie浏览器登录腾讯视频 进入签到页面 如下 浏览器地址栏输入javascript:alert(document.cookie) 即可获得所需cookie
+var cookie = " " //cookie浏览器登录腾讯视频进入签到页面如下浏览器地址栏输入javascript:alert(document.cookie)当然fd或者f12都可.只不过这个电脑手机都可以 即可获得所需cookie
 var url = "https://vip.video.qq.com/fcgi-bin/comm_cgi?name=hierarchical_task_system&cmd=2"; //签到地址
 res = http.post(url,{},{headers:{"cookie":cookie}});
 log(res.body.string());
