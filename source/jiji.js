911f9ffa96b06ad9544bbc99e0d7c2d2c6a9620c
Wenmoux <45694869+Wenmoux@users.noreply.github.com>
2019-11-07 11:28:49 +0800

    Update jiji.js

--- source/jiji.js
+++ source/jiji.js
@@ -4,7 +4,7 @@ var url = "https://ji-c.pw/user/checkin";
 var cookie = cookie1;
 var headers = {
 "User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.87 Safari/537.36";
-"cookie":cookie
+"cookie":cookie1
 };
 
 res = http.post(url,{},headers);
